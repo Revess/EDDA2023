3def6417ecd99bed43363ce4e65a6bd0dd7ca7b8
FredAlfabetAdmin <admin@fredalfabet.nl>
2023-04-15 12:39:20 +0200

    adds the resit folder

---
+++ ass1 resit/e1.R
@@ -0,0 +1,156 @@
+#~~~~A~~~~#
+df = read.csv("~/projects/EDDA2023/ass1/datasets/birthweight.txt")
+df = read.csv("C:/Users/markd/Code/EDDA2023/ass1/datasets/birthweight.txt")
+
+#The First sentence
+qqnorm(df$birthweight)
+
+mean(df$birthweight)
+var(df$birthweight)
+max(df$birthweight)
+min(df$birthweight)
+qnorm(0.96)
+
+total = 0
+for (i in 1:length(df$birthweight))
+{
+  total = total + (mean(df$birthweight) - df$birthweight[i])^2
+} 
+print(total)
+
+print(total/(length(df$birthweight)-1))
+
+#The CI
+t.test(df$birthweight,conf.level=0.96)
+
+#Bootstrapping
+B=206
+CI=0.96
+a=1-CI
+Tstar=numeric(B)
+c1 = df$birthweight
+for(i in 1:B) {
+    Xstar=sample(c1,replace=TRUE)
+    Tstar[i]=mean(Xstar) 
+}
+
+L=quantile(Tstar,a)
+U=quantile(Tstar,1-(a))
+sum(Tstar<L)
+c(2*mean(c1)-U,2*mean(c1)-L)
+
+
+#~~~~B~~~~#
+t.test(df$birthweight, mu=2800, alternative = "greater")
+#We reject the null hypothesis, 0.01357<0.05
+binom.test(sum(df$birthweight>2800), n=nrow(df), alternative = "greater")
+#We reject the null hypothesis, 0.03399<0.05
+
+#~~~~C~~~~#
+binom.test(sum(df$birthweight<2800), n=nrow(df), alternative = "greater")
+1-binom.test(sum(df$birthweight<2800), n=nrow(df), alternative = "greater")$p.value
+
+power.t.test(n = length(df$birthweight), delta = (2800 - mean(df$birthweight)) / sd(df$birthweight), sd = sd(df$birthweight), sig.level = 0.05, type = "one.sample", alternative = "greater")
+
+B=1000; n=length(df$birthweight)
+psign=numeric(B) ## will contain p-values of sign test
+pttest=numeric(B) ## will contain p-values of t-test
+for(i in 1:B) {
+x=rnorm(n,mean=0.5,sd=1) ## generate data under H1 with mu=0.5
+pttest[i]=t.test(x)[[3]] ## extract p-value
+psign[i]=binom.test(sum(x>0),n,p=0.5)[[3]] } ## extract p-value
+sum(psign<0.05)/B # fraction of rejecting H0, the power of the sign test
+sum(pttest<0.05)/B # fraction of rejecting H0, the power of the t-test
+
+############################
+# This was some testing, ignore:
+#binom.test(sum(df$birthweight<2600), n=nrow(df), p=0.025)
+#sum(df$birthweight<2600)
+#r = 0.25 + (1.96 * sqrt((0.25*0.75)/sum(df$birthweight<2600)))
+#l = 0.25 - (1.96 * sqrt((0.25*0.75)/sum(df$birthweight<2600)))
+#sqrt((r-l)/2)
+#(r-l)/2/2
+
+
+#lower = subset(df, birthweight < 2600)
+#prob = length(lower$birthweight) / length(df$birthweight)
+#prob
+#mean(lower$birthweight)
+
+
+#new_mean = mean(df$birthweight) * (2*prob)
+#new_mean
+#binom.test(lower, n=nrow(lower$birthweight), p=0.25)
+
+
+#binom.test(ll, lb, p=0.5)
+#binom.test(length(subset(df, birthweight < 2600)$birth), length(df$birthweight), alternative="less", conf.level=0.25)
+
+############################
+#~~~~D~~~~#
+# Calculate the PR based on PR
+lower = subset(df, birthweight < 2600)$birthweight
+sample_mean_amount = mean(lower)
+sample_mean_amount
+sample_mean_prob = length(lower) / length(df$birthweight)
+sample_mean_prob
+pl = 0.25
+pr = sample_mean_prob + (sample_mean_prob - pl)
+pl
+pr
+
+mean_all = mean(df$birthweight)
+mean_all
+cl_left = mean_all * (2 * pl)
+cl_right = mean_all * (2 * pr)
+cl_left
+cl_right
+
+result = (cl_left + cl_right) / 2
+result
+###############################
+
+#~~~~E~~~~#
+# sd(df$birthweight)
+# mean(df$birthweight)
+
+# pvals = c()
+
+# for(i in 1:5000){
+#     m2600 = sample(df[df < 2600], 34)
+#     f2600 = sample(df[df < 2600 & df != m2600], 28)
+
+#     males = sample(df[df >= 2600], 61)
+#     females = sample(df[df >= 2600 & df != males], 65)
+
+#     males = c(m2600,males)
+#     females = c(f2600,females)
+#     pvals = c(pvals, t.test(males,females)$p.value)
+# }
+
+# mean(pvals)
+# hist(pvals)
+
+malemeans = c()
+femalemeans = c()
+for(i in 1:5000){
+    m2600 = sample(df[df < 2600], 34)
+    f2600 = sample(df[df < 2600 & df != m2600], 28)
+
+    males = sample(df[df >= 2600], 61)
+    females = sample(df[df >= 2600 & df != males], 65)
+
+    males = c(m2600,males)
+    females = c(f2600,females)
+    malemeans= c(malemeans,mean(males))
+    femalemeans= c(femalemeans,mean(females))
+}
+
+t.test(malemeans,femalemeans)
+
+hist(malemeans)
+hist(femalemeans)
+
+mean(malemeans)
+mean(femalemeans)
+
---
+++ ass1 resit/e3.R
@@ -0,0 +1,111 @@
+'''
+To investigate the effect of 3 types of diet, 78 persons were divided randomly in 3 groups, 
+the first group following diet 1, second group diet 2 and the third group diet 3. 
+Next to some other characteristics, the weight was measured before diet and after 6 weeks 
+of diet for each person in the study. The collected data is summarized in the 
+data frame diet.txt Download diet.txtwith the following columns: 
+person - participant number, 
+gender - gender (1 = male, 0 = female), 
+age - age (years), 
+height - height (cm), 
+preweight - weight before the diet (kg), 
+diet - the type of diet followed, 
+weight6weeks - weight after 6 weeks of diet (kg). 
+Compute and add to the data frame the variable weight.lost expressing the lost weight, 
+to be used as response variable.
+
+a)  Make an informative graphical summary of the data relevant for study of the effect of diet 
+on the wight loss. By using only the columns preweight and weight6weeks, test the claim that the 
+diet affects the weight loss. Check the assumptions of the test applied.
+
+b)  Apply one-way ANOVA to test whether type of diet has an effect on the lost weight. Do all three types 
+diets lead to weight loss? Which diet was the best for losing weight? Can the Kruskal-Wallis test be applied 
+for this situation?
+
+c)  Use two-way ANOVA to investigate effect of the diet and gender (and possible interaction) on the lost weight.
+
+d)  Apply an appropriate model to investigate effects of diet and height (and possibly their interaction) on the 
+lost weight. Is the effect of height the same for all 3 types of diet?
+
+e)  Which of the two approaches, the one from b) or the one from d), do you prefer? Why? For the preferred model, 
+predict the lost weight for all three types of diet for an average person.
+'''
+
+df = read.csv("~/projects/EDDA2023/ass1/datasets/diet.txt", sep="\t")
+df$weightdiff = c(df["preweight"] - df["weight6weeks"])$preweight
+#~~~A~~~#
+# Then somehow combine these into a histogram
+t.test(df[df$diet == 1,]$preweight, df[df$diet == 1,]$weight6weeks)
+'''data:  df[df$diet == 1, ]$preweight and df[df$diet == 1, ]$weight6weeks
+t = 1.3623, df = 46, p-value = 0.1797
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -1.575829  8.175829
+sample estimates:
+mean of x mean of y 
+   72.875    69.575'''
+
+t.test(df[df$diet == 2,]$preweight, df[df$diet == 2,]$weight6weeks)
+'''data:  df[df$diet == 2, ]$preweight and df[df$diet == 2, ]$weight6weeks
+t = 1.0948, df = 51.992, p-value = 0.2787
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ -2.520349  8.572201
+sample estimates:
+mean of x mean of y 
+ 71.11111  68.08519'''
+
+t.test(df[df$diet == 3,]$preweight, df[df$diet == 3,]$weight6weeks)
+'''data:  df[df$diet == 3, ]$preweight and df[df$diet == 3, ]$weight6weeks
+t = 2.385, df = 51.668, p-value = 0.02078
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ 0.816050 9.480246
+sample estimates:
+mean of x mean of y 
+ 73.62963  68.48148'''
+
+# OR IT IS JUST ONE TEST????
+t.test(df$preweight, df$weight6weeks)  ##And is the alt less than? , alt="less"
+'''data:  df$preweight and df$weight6weeks
+t = 2.721, df = 153.92, p-value = 0.007259
+alternative hypothesis: true difference in means is not equal to 0
+95 percent confidence interval:
+ 1.053396 6.636348
+sample estimates:
+mean of x mean of y 
+ 72.52564  68.68077'''
+
+#~~~B~~~#
+aov(weightdiff ~ diet, data=df)
+kruskal.test(weightdiff ~ diet, data=df)
+
+#~~~C~~~#
+aov(weightdiff ~ gender + age, data=df)
+
+#~~~D~~~#
+kruskal.test(df[df$diet == 1,]$weightdiff ~ df[df$diet==1,]$height)
+kruskal.test(df[df$diet == 2,]$weightdiff ~ df[df$diet==2,]$height)
+kruskal.test(df[df$diet == 3,]$weightdiff ~ df[df$diet==3,]$height)
+# BC it gives a p-value
+
+#~~~E~~~#
+# BC it gives a p-value
+kruskal.test(df[df$diet == 1,]$preweight ~ df[df$diet==1,]$weight6weeks)
+kruskal.test(df[df$diet == 2,]$preweight ~ df[df$diet==2,]$weight6weeks)
+kruskal.test(df[df$diet == 3,]$preweight ~ df[df$diet==3,]$weight6weeks)
+
+diet1 = df[df$diet == 1,]
+diet2 = df[df$diet == 2,]
+diet3 = df[df$diet == 3,]
+mean(diet1$weightdiff, na.rm=TRUE)
+mean(diet2$weightdiff, na.rm=TRUE)
+mean(diet3$weightdiff, na.rm=TRUE)
+
+summary(lm(weightdiff ~ diet, data=diet1))
+summary(lm(weightdiff ~ diet, data=diet2))
+summary(lm(weightdiff ~ diet, data=diet3))
+
+
+
+summary(mean_sales = mean(df$weightdiff, na.rm=TRUE))
